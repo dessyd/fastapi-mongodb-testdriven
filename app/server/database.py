15bc9a1fbf38380747dd44f8517bb6ca9f8c5b94
Dominique Dessy <dominique.dessy@gmail.com>
2021-12-15 10:42:49 +0100

    Corrected GPA -> gpa

--- app/server/database.py
+++ app/server/database.py
@@ -22,7 +22,7 @@ def student_helper(student) -> dict:
         "email": student["email"],
         "course_of_study": student["course_of_study"],
         "year": student["year"],
-        "GPA": student["gpa"],
+        "gpa": student["gpa"],
     }
 
 # Retrieve all students present in the database
--- app/server/routes/students.py
+++ app/server/routes/students.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Body, Response, status, HTTPException
 from fastapi.encoders import jsonable_encoder
+from typing import List
 
 from ..database import (
     add_student,
@@ -26,20 +27,20 @@ async def add_student_data(student: StudentSchema = Body(...)):
     new_student = await add_student(student)
     return new_student
 
-@router.get("/", response_description="Students retrieved")
+@router.get("/", response_model=List[StudentSchema], response_description="Students retrieved")
 async def get_students():
     students = await retrieve_students()
-    if students:
-        return ResponseModel(students, "Students data retrieved successfully")
-    return ResponseModel(students, "Empty list returned")
+    print(students)
+    return students
 
 
 @router.get("/{id}", response_description="Student data retrieved")
 async def get_student_data(id):
     student = await retrieve_student(id)
-    if student:
-        return ResponseModel(student, "Student data retrieved successfully")
-    return ErrorResponseModel("An error occurred.", 404, "Student doesn't exist.")
+    if not student:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"Student with id: {id} is unknown")
+    return student
 
 @router.put("/{id}")
 async def update_student_data(id: str, req: UpdateStudentModel = Body(...)):
@@ -59,9 +60,10 @@ async def update_student_data(id: str, req: UpdateStudentModel = Body(...)):
 @router.delete("/{id}", response_description="Student data deleted from the database")
 async def delete_student_data(id: str):
     deleted_student = await delete_student(id)
-    if deleted_student:
-        return ResponseModel(
-            "Student with ID: {} removed".format(id), "Student deleted successfully"
-        )
-    return ErrorResponseModel(
-        "An error occurred", 404, "Student with id {0} doesn't exist".format(id))
+
+    if not deleted_student:
+       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                    detail=f"Student with id: {id} is unknown")
+
+    return ResponseModel(
+        "Student with ID: {} removed".format(id), "Student deleted successfully")
